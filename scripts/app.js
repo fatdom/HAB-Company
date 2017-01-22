8d2bfad630f48803fd95e6d7f3fa9ffbb1d75f06
fatdom <fatdom@gmail.com>
2017-01-22 15:49:23 +0800

    HTML Skeleton with FullPage.js

---
+++ scripts/app.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        paddingRight: '100px',
+        anchors: ['main', 'about', 'works', 'service', 'contact'],
+        menu: '#nav-bottom  ',
+        slidesNavigation: false,
+        controlArrows: false,
+        verticalCentered: false,
+        afterLoad: function(){
+            $("#nav-bottom .nav-img").attr('src','images/point-off.png')
+            $("#nav-bottom .active").attr('src','images/point-on.png');
+        }
+    });
+
+    $('.menu-main-1').hover(
+        function(){
+            this.setAttribute('src', 'images/main/P1-cover-button-about-on.png');
+        },
+        function(){
+            this.setAttribute('src', 'images/main/P1-cover-button-about-off.png');
+        }
+    );
+    
+    $('.menu-main-2').hover(
+        function(){
+            this.setAttribute('src', 'images/main/P1-cover-button-work-on.png');
+        },
+        function(){
+            this.setAttribute('src', 'images/main/P1-cover-button-work-off.png');
+        }
+    );
+    
+    $('.menu-main-3').hover(
+        function(){
+            this.setAttribute('src', 'images/main/P1-cover-button-service-on.png');
+        },
+        function(){
+            this.setAttribute('src', 'images/main/P1-cover-button-service-off.png');
+        }
+    );
+    
+    $('.menu-main-4').hover(
+        function(){
+            this.setAttribute('src', 'images/main/P1-cover-button-contact-on.png');
+        },
+        function(){
+            this.setAttribute('src', 'images/main/P1-cover-button-contact-off.png');
+        }
+    );
+});
